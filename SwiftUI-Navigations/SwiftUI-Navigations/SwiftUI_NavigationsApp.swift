28b8b8be17276690060f310e8cda21cbae13bcda
Viktor Holovach <viktorgolovach@Viktors-MacBook-Pro.local>
2023-09-05 07:51:55 +0200

    initial commit

---
+++ SwiftUI-Navigations/SwiftUI-Navigations/SwiftUI_NavigationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavigationsApp.swift
+//  SwiftUI-Navigations
+//
+//  Created by Viktor Golovach on 05.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavigationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
